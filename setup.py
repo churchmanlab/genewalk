2a0ac80dd7223b604d700f89d65a4176263a1cb3
Ben Gyori <ben.gyori@gmail.com>
2019-02-05 17:14:59 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import sys
+from setuptools import setup
+
+def main():
+    install_list = ['numpy', 'pandas', 'networkx>=2.1', 'gensim', 'goatools']
+
+    setup(name='ngf',
+          version='0.0.1',
+          description='Determine gene function based on network embeddings.',
+          long_description=(''),
+          author='Robert Ietswaart',
+          author_email='robert_ietswaart@hms.harvard.edu',
+          url='https://github.com/churchmanlab/NGF',
+          classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
+            'Topic :: Scientific/Engineering :: Bio-Informatics',
+            ],
+          keywords=['gene function', 'network', 'embedding'],
+          packages=['ngf'],
+          install_requires=install_list,
+          extras_require={'indra': ['indra']}
+          tests_require=['nose'],
+          include_package_data=True,
+        )
+
+
+if __name__ == '__main__':
+    main()
