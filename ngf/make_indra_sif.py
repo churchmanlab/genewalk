c0a48676563400bfda2cbdd66cdd06b93f644e06
Ben Gyori <ben.gyori@gmail.com>
2019-02-05 22:26:43 -0500

    Use pickle output instead of SIF

--- ngf/make_indra_sif.py
+++ ngf/make_indra_sif.py
@@ -4,9 +4,15 @@ interactions as a pandas DataFrame and, filters it to the genes of interest,
 and adds some additional information before exporting as SIF."""
 import pandas
 import pickle
+import logging
 import argparse
+from indra.util import batch_iter
+from indra.sources import indra_db_rest
 from indra.databases import hgnc_client
 from indra.preassembler.hierarchy_manager import hierarchies
+
+
+logger = logging.getLogger('ngf.make_indra_sif')
 
 
 def load_genes(fname):
@@ -15,6 +21,7 @@ def load_genes(fname):
         # Get the HGNC IDs from the list of genes file, assuming that
         # each line looks like HGNC:123
         genes = [l.strip().split(':')[1] for l in fh.readlines()]
+    logger.info('Loaded %d genes from %s' % (len(genes), fname))
     return genes
 
 
@@ -22,12 +29,15 @@ def load_indra_df(fname):
     """Return an INDRA Statement data frame from a pickle file."""
     with open(fname, 'rb') as fh:
         df = pickle.load(fh)
+    logger.info('Loaded %d rows from %s' % (len(df), fname))
     return df
 
 
-def dump_sif(df, fname):
-    """Dump a data frame into a CSV file."""
-    df.to_csv(fname)
+def dump_pickle(stmts, fname):
+    """Dump a list of Statements into a picke file."""
+    with open(fname, 'wb') as fh:
+        pickle.dump(stmts, fh)
+    logger.info('Dumped %d statements into %s' % (len(stmts), fname))
 
 
 def filter_to_genes(df, genes, fplx_terms):
@@ -42,7 +52,9 @@ def filter_to_genes(df, genes, fplx_terms):
                      ((df.agB_ns == 'FPLX') & (df.agB_id.isin(fplx_terms))) |
                      (df.agB_ns == 'GO'))
     # sources/targets
-    return df[source_filter & target_filter]
+    df = df[source_filter & target_filter]
+    logger.info('Filtered data frame to %d rows.' % len(df))
+    return df
 
 
 def get_famplex_terms(genes):
@@ -55,15 +67,22 @@ def get_famplex_terms(genes):
         parents = eh.get_parents(gene_uri)
         parent_ids = [eh.ns_id_from_uri(par_uri)[1] for par_uri in parents]
         all_parents |= set(parent_ids)
-    return sorted(list(all_parents))
+    fplx_terms = sorted(list(all_parents))
+    logger.info('Found %d relevant FamPlex terms.' % (len(fplx_terms)))
+    return fplx_terms
 
 
-def collapse_and_count(df):
-    """Collapse an INDRA Statement data frame and count evidences."""
-    df_counts = df.groupby(by=['agA_ns', 'agA_id', 'agA_name',
-                               'agB_ns', 'agB_id', 'agB_name',
-                               'stmt_type']).sum()
-    return df_counts
+def download_statements(df):
+    """Download the INDRA Statements corresponding to entries in a data frame.
+    """
+    from indra.sources.indra_db_rest.util import logger
+    logger.setLevel(logging.ERROR)
+    all_stmts = []
+    for idx, group in enumerate(batch_iter(df.hash, 500)):
+        logger.info('Getting statement batch %d' % idx)
+        stmts = indra_db_rest.get_statements_by_hash(list(group))
+        all_stmts += stmts
+    return all_stmts
 
 
 if __name__ == '__main__':
@@ -72,7 +91,7 @@ if __name__ == '__main__':
         description='Choose a file with a list of genes to get a SIF for.')
     parser.add_argument('--genes', default='data/JQ1_HGNCidForINDRA.csv')
     parser.add_argument('--indra_df', default='data/stmt_df.pkl')
-    parser.add_argument('--sif', default='data/JQ1_HGNCidForINDRA.sif')
+    parser.add_argument('--pickle', default='data/JQ1_HGNCidForINDRA.pkl')
     args = parser.parse_args()
     # Load genes and get FamPlex terms
     genes = load_genes(args.genes)
@@ -81,6 +100,7 @@ if __name__ == '__main__':
     df = load_indra_df(args.indra_df)
     # Filter the data frame to relevant entities
     df = filter_to_genes(df, genes, fplx_terms)
-    # Collapse data frame and dump into SIF
-    df = collapse_and_count(df)
-    dump_sif(df, args.sif)
+    # Download the Statement corresponding to each row
+    stmts = download_statements(df)
+    # Dump the Statements into a pickle file
+    dump_pickle(stmts, args.pickle)
