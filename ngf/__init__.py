5beed6ab779d793872360042f453abe09afe9dd0
Ben Gyori <ben.gyori@gmail.com>
2019-02-05 22:26:22 -0500

    Add logging to module

---
+++ ngf/__init__.py
@@ -0,0 +1,5 @@
+import logging
+
+logging.basicConfig(format=('%(levelname)s: [%(asctime)s] %(name)s'
+                            ' - %(message)s'),
+                    level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
