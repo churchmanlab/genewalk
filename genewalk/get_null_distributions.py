a5b4d23ddbce6d402df646288f1ea846826dce15
ri23 <robertietswaart@gmail.com>
2019-02-27 16:38:59 -0500

    added code in main where null dists are generated from random node vectors

--- genewalk/get_null_distributions.py
+++ genewalk/get_null_distributions.py
@@ -37,27 +37,27 @@ if __name__ == '__main__':
     parser.add_argument('--Nreps', default=15)
     args = parser.parse_args()
 
+    srd=dict() #similarity (random) null distributions 
+    #key structure: of srd: 'd'+str(#connections of a node)
+
     #load multigraph
     with open(args.path+args.mg, 'rb') as f:
         MGA=pkl.load(f)
 
     for rep in range(1,args.Nreps+1):
+        print(rep,'/',args.Nreps)
         #generate random graph
         random.seed(a=None)
         gseed=random.randint(0, 1e12)
         print('gseed',gseed)
         rg=Get_Rand_Graph(MGA,gseed)
         
-        #initialize DW object
         DW=DeepWalk(graph=rg)
-        del(rg)
-
         print('generate random walks', rep)
         start = time.time()
         DW.get_walks()
         end = time.time()
         print('DW.get_walks done',end - start)#in sec
-
         #pickling to save walks
         filename='GeneWalk_DW_rand_'+rep+'.pkl'
         with open(args.path+filename, 'wb') as f:
@@ -68,13 +68,40 @@ if __name__ == '__main__':
         DW.word2vec()
         end = time.time()
         print('DW.word2vec ',end - start)#in sec
-
         ##Pickle the node vectors (embeddings) and DeepWalk object
         nv = copy.deepcopy(DW.model.wv)
         filename='GeneWalk_DW_nv_rand_'+rep+'.pkl'
         with open(args.path+filename, 'wb') as f:
             pkl.dump(nv,f)
-
         filename='GeneWalk_DW_rand_'+rep+'.pkl'
         with open(args.path+filename, 'wb') as f:
             pkl.dump(DW,f)
+        del(DW)
+    
+        #Generate null distributions from random node vectors
+        g_view=nx.nodes(rg)
+        for node in g_view:
+            connections=list(rg[node])
+            if node in connections:#only get sims for non self connections
+                connections.remove(node)
+            N_con_source=len(connections)
+            sim_dist=list( 1-nv.distances(node, other_words=connections) )
+            for inb in range(len(connections)):
+                nb=connections[inb]
+                N_con_neighbor=len(rg[nb])
+                #make log2 scale categories of connectivity
+                key='d'+str(np.floor(np.log2(min(N_con_source,N_con_neighbor))))
+                if key in srd:
+                    srd[key].append(sim_dist[inb])
+                else:
+                    srd[key]=[sim_dist[inb]]
+
+    #To improve search speed: convert lists with similarities to sorted numpy arrays.
+    for skey in srd.keys():
+        srd[skey]=np.asarray(sorted(srd[skey]))
+            
+    #Pickle srd for use in genewalk.py
+    filename='GeneWalk_DW_rand_simdists.pkl'
+    print(filename)     
+    with open(args.path+filename, 'wb') as f:
+        pkl.dump(srd,f)
