72a5e825be894fe0f72a57727e4dd70b2760981a
ri23 <robertietswaart@gmail.com>
2019-06-20 18:54:18 -0400

    added get_pc, renamed download_goa to download_gz

--- genewalk/resources.py
+++ genewalk/resources.py
@@ -23,16 +23,15 @@ def download_go(fname):
     urllib.request.urlretrieve(url, fname)
 
 
-def download_goa(fname):
-    url = 'http://geneontology.org/gene-associations/goa_human.gaf.gz'
+def download_gz(fname,url):
     logger.info('Downloading %s and extracting into %s' % (url, fname))
-    gz_file = os.path.join(resource_dir, 'goa_human.gaf.gz')
+    gz_file = os.path.join(resource_dir, fname+'.gz')
     urllib.request.urlretrieve(url, gz_file)
     with gzip.open(gz_file, 'rb') as fin:
         with open(fname, 'wb') as fout:
             shutil.copyfileobj(fin, fout)
 
-
+    
 def get_go_obo():
     fname = os.path.join(resource_dir, 'go.obo')
     if not os.path.exists(fname):
@@ -43,5 +42,14 @@ def get_go_obo():
 def get_goa_gaf():
     fname = os.path.join(resource_dir, 'goa_human.gaf')
     if not os.path.exists(fname):
-        download_goa(fname)
+        url_goa = 'http://geneontology.org/gene-associations/goa_human.gaf.gz'
+        download_gz(fname,url_goa)
+    return fname
+
+
+def get_pc():
+    fname = os.path.join(resource_dir, 'PathwayCommons11.All.hgnc.sif')
+    if not os.path.exists(fname):
+        url_pc = 'http://www.pathwaycommons.org/archives/PC2/v11/PathwayCommons11.All.hgnc.sif.gz'
+        download_gz(fname,url_pc)
     return fname
