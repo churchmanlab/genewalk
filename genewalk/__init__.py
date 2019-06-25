6947b48eba6b1953d4acdedaafbc81e9db12426c
ri23 <robertietswaart@gmail.com>
2019-06-24 20:00:45 -0400

    unfinished: add fileHandler commented out

--- genewalk/__init__.py
+++ genewalk/__init__.py
@@ -2,6 +2,7 @@ import logging
 
 logging.basicConfig(format=('%(levelname)s: [%(asctime)s] %(name)s'
                             ' - %(message)s'),
-                    level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+                    level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')#,
+#                     handlers=logging.FileHandler('/n/groups/churchman/ri23/test.log'))#somehow this does not work yet
 
 __version__ = '0.0.1'
