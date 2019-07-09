72683ec50ec12bc6c2b685338e6d802c38e48fbe
Ben Gyori <ben.gyori@gmail.com>
2019-07-09 00:25:59 -0400

    Implement getting all row elements

--- genewalk/perform_statistics.py
+++ genewalk/perform_statistics.py
@@ -75,6 +75,8 @@ class GeneWalk(object):
         go_attribs = []
         for go_node_id, sim_score in connected_similar:
             go_attrib = {}
+            go_attrib['sim_score'] = sim_score
+            go_attrib['go_id'] = go_node_id
             go_attrib['ncon_go'] = self.graph[go_node_id]
             go_attrib['go_name'] = self.graph[go_node_id]['name']
             go_attrib['pval'] = \
@@ -99,12 +101,43 @@ class GeneWalk(object):
             terms are output). If set to a lower value, only annotated GO
             terms with mean padj < alpha_FDR are output.
         """
-        for nv in self.nvs:
-            g_view = nx.nodes(self.graph)
-            for node in nx.nodes(self.graph):
-                if self.graph.node[n]['HGNC'] in self.gene_nodes:
-                    gene_attribs = self.get_gene_attribs(node)
-                    go_attribs = self.get_go_attribs(gene_attribs, nv)
+        rows = []
+        for node in self.gene_nodes:
+            gene_attribs = self.get_gene_attribs(node)
+            all_go_attribs = [self.get_go_attribs(gene_attribs, nv) for nv in
+                              self.nvs]
+            go_attrib_dict = {}
+            for go_attrib_list in all_go_attribs:
+                for go_attribs in go_attrib_list:
+                    if go_attribs['go_id'] in go_attrib_dict:
+                        go_attrib_dict[go_attribs['go_id']].append(go_attribs)
+                    else:
+                        go_attrib_dict[go_attribs['go_id']] = [go_attribs]
+
+            for go_id, go_attribs in go_attrib_dict.items()
+                mean_sim = np.mean([attr['sim_score'] for attr in go_attribs])
+                ste_sim = (np.std([attr['sim_score'] for attr in go_attribs]) /
+                           np.sqrt(len(self.nvs)))
+                mean_pval = np.mean([attr['pval'] for attr in go_attribs])
+                ste_pval = (np.std([attr['pval'] for attr in go_attribs]) /
+                            np.sqrt(len(self.nvs)))
+                mean_qval = np.mean([attr['qval'] for attr in go_attribs])
+                ste_qval = (np.std([attr['qval'] for attr in go_attribs]) /
+                            np.sqrt(len(self.nvs)))
+                row = [gene_attribs['hgnc_symbol'],
+                       gene_attribs['hgnc_id'],
+                       go_attribs['go_name'],
+                       go_attribs['go_id'],
+                       gene_attribs['ncon_gene'],
+                       go_attribs['ncon_go'],
+                       mean_sim, ste_sim,
+                       mean_pval, ste_pval,
+                       mean_qval, ste_qval]
+                rows.append(row)
+
+
+
+
 
         #Merge all self.Nrep experimentals to calculate mean and sem statistics
         self.outdfs[self.Nreps+1]=copy.deepcopy(self.outdfs[1])
