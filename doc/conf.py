9f929b11b2e07049e0d9c7e44beaab5ffcf4abaf
Ben Gyori <ben.gyori@gmail.com>
2019-08-30 09:19:59 -0400

    Increase versions in init and docs

--- doc/conf.py
+++ doc/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, GeneWalk Developers'
 author = 'GeneWalk Developers'
 
 # The short X.Y version
-version = ''
+version = '1.0'
 # The full version, including alpha/beta/rc tags
-release = ''
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
--- genewalk/__init__.py
+++ genewalk/__init__.py
@@ -10,4 +10,4 @@ logging.basicConfig(format=default_logger_format, level=logging.INFO,
 logger = logging.getLogger('genewalk')
 
 
-__version__ = '0.0.1'
+__version__ = '1.0.0'
