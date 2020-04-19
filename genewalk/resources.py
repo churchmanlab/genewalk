387eb0faa4288fb0be0adb99ccd6985d5545456d
ri23 <robertietswaart@gmail.com>
2020-04-18 21:12:03 -0400

    added functionality to replace outdated with current hgnc symbols in PC v12

--- genewalk/resources.py
+++ genewalk/resources.py
@@ -3,6 +3,8 @@ import gzip
 import shutil
 import logging
 import urllib.request
+import pandas
+from indra.databases import hgnc_client
 
 logger = logging.getLogger('genewalk.resources')
 
@@ -31,13 +33,17 @@ class ResourceManager(object):
         return fname
 
     def get_pc(self):
-        fname = os.path.join(self.resource_folder,
-                             'PathwayCommons12.All.hgnc.sif')
-        if not os.path.exists(fname):
-            url_pc = ('http://www.pathwaycommons.org/archives/PC2/v12/'
-                      'PathwayCommons12.All.hgnc.sif.gz')
-            download_gz(fname, url_pc)
-        return fname
+        fname_current = os.path.join(self.resource_folder,
+                                     'PathwayCommons12.All.hgnc_current.sif')
+        if not os.path.exists(fname_current):
+            fname = os.path.join(self.resource_folder,
+                                 'PathwayCommons12.All.hgnc.sif')
+            if not os.path.exists(fname):
+                url_pc = ('http://www.pathwaycommons.org/archives/PC2/v12/'
+                          'PathwayCommons12.All.hgnc.sif.gz')
+                download_gz(fname, url_pc)
+            self._replace_outdated_hgnc_symbols(fname,fname_current)
+        return fname_current
 
     def get_mgi_entrez(self):
         fname = os.path.join(self.resource_folder, 'MGI_EntrezGene.rpt')
@@ -49,7 +55,6 @@ class ResourceManager(object):
 
     def _get_resource_folder(self):
         resource_dir = os.path.join(self.base_folder, 'resources')
-
         if not os.path.isdir(resource_dir):
             try:
                 os.makedirs(resource_dir)
@@ -57,12 +62,39 @@ class ResourceManager(object):
                 logger.warning(resource_dir + ' already exists')
         return resource_dir
 
+    def _replace_outdated_hgnc_symbols(self,pc_old,pc_current):
+        logger.info('Replacing outdated HGNC symbols in %s and save as %s' % \
+                    (pc_old, pc_current))
+        pc = pandas.read_csv(pc_old,sep='\t',dtype=str, header=None)
+        col_mapper = {}
+        col_mapper[0] = 'source'
+        col_mapper[1] = 'rel_type'
+        col_mapper[2] = 'target'
+        pc = pc.rename(mapper=col_mapper, axis='columns')
+        all_symbols = set(pc['source']).union(pc['target'])
+        symbol_map = {}
+        for sym in all_symbols:
+            if not sym.startswith('CHEBI:'):
+                hgnc_id = hgnc_client.get_current_hgnc_id(sym)
+                if not hgnc_id:
+                    continue
+                elif isinstance(hgnc_id, list):
+                    #outdated gene symbol is ambiguous: maps to multiple genes
+                    continue
+                latest_symbol = hgnc_client.get_hgnc_name(hgnc_id)
+                if latest_symbol != sym:
+                    symbol_map[sym] = latest_symbol
+        if symbol_map:
+            pc.replace(symbol_map,inplace=True)         
+        pc.to_csv(pc_current, sep='\t', header=False, index=False)
+        os.remove(pc_old)
+        
     def download_all(self):
         self.get_go_obo()
         self.get_goa_gaf()
         self.get_pc()
         self.get_mgi_entrez()
-
+        
 
 def download_url(url, fname):
     logger.info('Downloading %s into %s' % (url, fname))
