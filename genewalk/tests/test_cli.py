0b5597a3df4627092a23bb9915a88e7d54584acf
Ben Gyori <ben.gyori@gmail.com>
2021-01-26 22:33:25 -0500

    Make test base folder

--- genewalk/tests/test_cli.py
+++ genewalk/tests/test_cli.py
@@ -4,11 +4,12 @@ import shutil
 from genewalk.cli import run_main, default_base_folder
 
 HERE = os.path.dirname(os.path.abspath(__file__))
+TEST_BASE_FOLDER = os.path.join(default_base_folder, '.test')
 
 
 class ArgparseMock:
     def __init__(self, project, genes, id_type,
-                 stage='all', base_folder=default_base_folder,
+                 stage='all', base_folder=TEST_BASE_FOLDER,
                  network_source='pc', network_file=None, nproc=1,
                  nreps_graph=3, nreps_null=3, alpha_fdr=1.0,
                  dim_rep=8, save_dw=False, random_seed=None):
@@ -32,19 +33,19 @@ def place_test_files():
     test_resource_files = glob.glob(os.path.join(HERE, 'resources', '*'))
     for test_file in test_resource_files:
         shutil.copy(test_file,
-                    os.path.join(default_base_folder,
+                    os.path.join(TEST_BASE_FOLDER,
                                  'resources', os.path.basename(test_file)))
 
 
 def test_default():
     project_name = 'test1'
-    gene_list = os.path.join(HERE, 'hgnc_symbols.txt')
+    gene_list = os.path.join(HERE, 'resources', 'hgnc_symbols.txt')
     args = ArgparseMock(project_name, gene_list, 'hgnc_symbol')
 
     place_test_files()
 
     run_main(args)
 
-    assert os.path.exists(default_base_folder)
-    assert os.path.exists(os.path.join(default_base_folder, project_name,
+    assert os.path.exists(TEST_BASE_FOLDER)
+    assert os.path.exists(os.path.join(TEST_BASE_FOLDER, project_name,
                                        'genewalk_results.csv'))
