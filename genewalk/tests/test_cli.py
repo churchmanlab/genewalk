04ae71281824f9168019941e7112b3cc3e7bc99a
Ben Gyori <ben.gyori@gmail.com>
2021-01-26 21:14:46 -0500

    Add initial CLI test

---
+++ genewalk/tests/test_cli.py
@@ -0,0 +1,36 @@
+import os
+from genewalk.cli import run_main, default_base_folder
+
+
+class ArgparseMock:
+    def __init__(self, project, genes, id_type,
+                 stage='all', base_folder=default_base_folder,
+                 network_source='pc', network_file=None, nproc=1,
+                 nreps_graph=3, nreps_null=3, alpha_fdr=1.0,
+                 dim_rep=8, save_dw=False, random_seed=None):
+        self.project = project
+        self.genes = genes
+        self.id_type = id_type
+        self.stage = stage
+        self.base_folder = base_folder
+        self.network_source = network_source
+        self.network_file = network_file
+        self.nproc = nproc
+        self.nreps_graph = nreps_graph
+        self.nreps_null = nreps_null
+        self.alpha_fdr = alpha_fdr
+        self.dim_rep = dim_rep
+        self.save_dw = save_dw
+        self.random_seed = random_seed
+
+
+def test_default():
+    project_name = 'test1'
+    gene_list = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                             'hgnc_symbols.txt')
+    args = ArgparseMock(project_name, gene_list, 'hgnc_symbol')
+    run_main(args)
+
+    assert os.path.exists(default_base_folder)
+    assert os.path.exists(os.path.join(default_base_folder, project_name,
+                                       'genewalk_results.csv'))
