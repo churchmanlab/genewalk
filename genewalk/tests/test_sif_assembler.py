50b2273cec3dedf0f6d0c64d0aa23617291a8e9a
Ben Gyori <ben.gyori@gmail.com>
2021-02-15 23:24:25 -0500

    Implement SIF assembler test with custom genes

--- genewalk/tests/test_sif_assembler.py
+++ genewalk/tests/test_sif_assembler.py
@@ -84,3 +84,35 @@ def test_full_sif():
     # SIF, not ones in the GO ontology
     assert ('GO:0000186', 'GO:0032147') not in mga.graph.edges
     assert ('GO:0000186', 'GO:0001934') in mga.graph.edges
+
+
+def test_custom_sif():
+    custom_genes = read_gene_list(
+        os.path.join(TEST_RESOURCES, 'custom_gene_list.txt'),
+        id_type='custom', resource_manager=rm)
+    sif = os.path.join(TEST_RESOURCES, 'test_sif_custom.sif')
+    mga = UserNxMgAssembler(custom_genes, resource_manager=rm,
+                            filepath=sif, gwn_format='sif_annot')
+
+    gene_nodes = {'CUSTOM:ABC', 'CUSTOM:XYZ', 'CUSTOM:ZZZ'}
+    # We still have all the GO nodes that are in the test GO OBO
+    # since we are in sif_annot mode here
+    go_nodes = {'GO:0001934', 'GO:0005515', 'GO:0000186', 'GO:0000001',
+                'GO:0032147', 'GO:0003924'}
+    graph_nodes = set(mga.graph.nodes())
+    assert graph_nodes == gene_nodes | go_nodes, graph_nodes
+
+    # Make sure we have GO node annotations as expected
+    go_node = mga.graph.nodes['GO:0000186']
+    assert go_node['GO'] == 'GO:0000186', go_node
+    assert go_node['domain'] == 'biological_process', go_node
+    assert go_node['name'] == 'activation of MAPKK activity', go_node
+
+    # Make sure we have gene-gene edges
+    assert ('CUSTOM:ABC', 'CUSTOM:XYZ') in mga.graph.edges
+
+    # Make sure we have GO annotation edges
+    assert ('CUSTOM:ABC', 'GO:0000186') in mga.graph.edges
+
+    # Make sure we have GO DAG edges
+    assert ('GO:0000186', 'GO:0032147') in mga.graph.edges
